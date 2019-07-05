5b9f2d2d6f7a6c6e2a6643089b3154736c73e9e9
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-05 15:45:53 +0200

    Redirect with localStorage

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage'
 import AppLayout from './AppLayout';
 import AppBar from './AppBar'
 import {AppProvider} from './AppProvider'
+import Settings from '../Settings/Settings'
 
 function App() {
   return (
       <AppLayout>
         <AppProvider>
           <AppBar/>
-          <Welcome />
+          <Settings />
         </AppProvider>
       </AppLayout>
   );
---
+++ src/Settings/Settings.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Welcome from './WelcomeMessage'
+import ConfirmButton from './ConfirmButton'
+
+export default function() {
+    return <div><Welcome/> <ConfirmButton/> </div>
+}
