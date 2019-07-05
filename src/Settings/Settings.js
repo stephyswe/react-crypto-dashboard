8eedd5c38511af49ceb04eeb85e2c98e27ef4d7c
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-05 17:48:35 +0200

    CoinGrid bottom complete

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Welcome from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
-import Coingrid from './CoinGrid'
+import CoinGrid from './CoinGrid'
 
 
 export default function() {
-    return <Page name="settings"><Welcome/> <ConfirmButton/> <Coingrid/> </Page>
+    return <Page name="settings"><Welcome/> <ConfirmButton/> <CoinGrid/> </Page>
 }
