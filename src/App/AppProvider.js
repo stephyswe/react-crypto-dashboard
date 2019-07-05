d4c398860798e3aab4cff94b65534fedce12a427
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-05 16:02:55 +0200

    Cryptocompare API and Page component

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cc from 'cryptocompare'
 
 export const AppContext = React.createContext();
 
@@ -12,6 +13,15 @@ export class AppProvider extends React.Component {
             confirmFavorites: this.confirmFavorites
 
         }
+    }
+
+    componentDidMount() {
+        this.fetchCoins();
+    }
+
+    fetchCoins = async () => {
+        let coinList = (await cc.coinList()).Data;
+        this.setState({coinList});
     }
 
     confirmFavorites = () => {
--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import Welcome from './WelcomeMessage'
-import ConfirmButton from './ConfirmButton'
+import React from 'react';
+import Welcome from './WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
+
 
 export default function() {
-    return <div><Welcome/> <ConfirmButton/> </div>
+    return <Page name="settings"><Welcome/> <ConfirmButton/> </Page>
 }
