896bf155a9ac5320b5265405fdcc4d4d51f5356f
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-05 18:51:58 +0200

    Finish Add/Remove Coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import _ from 'lodash';
 import cc from 'cryptocompare'
 
 export const AppContext = React.createContext();
+
+const MAX_FAVORITES = 10;
 
 export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             page: 'dashboard',
+            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
             ...this.savedSettings(),
             setPage: this.setPage,
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            isInFavorites: this.isInFavorites,
             confirmFavorites: this.confirmFavorites
         }
     }
@@ -24,13 +31,29 @@ export class AppProvider extends React.Component {
         this.setState({coinList});
     }
 
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if (favorites.length < MAX_FAVORITES) {
+            favorites.push(key);
+            this.setState({favorites});
+        }
+    };
+
+    isInFavorites = key => _.includes(this.state.favorites, key);
+
+    removeCoin = key => {
+        let favorites = [...this.state.favorites];
+        this.setState({favorites: _.pull(favorites, key)})
+
+    }
+
     confirmFavorites = () => {
         this.setState({
             firstVisit: false,
             page: 'dashboard'
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
-            test: 'hello'
+            favorites: this.state.favorites
         }))
     }
 
@@ -38,7 +61,8 @@ export class AppProvider extends React.Component {
         let cryptoDashData = JSON.parse(localStorage
             .getItem('cryptoDash'))
         if (!cryptoDashData) return {page: 'settings', firstVisit: true}
-        return {};
+        let {favorites} = cryptoDashData;
+        return {favorites};
     }
 
     setPage = page => this.setState({page})
