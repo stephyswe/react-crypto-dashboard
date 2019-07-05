f3f266199ec55d732b47cbb3f83c5d0f62b1e323
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-05 14:11:35 +0200

    Add AppProvider

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Welcome from './WelcomeMessage'
+import AppLayout from './AppLayout';
+import AppBar from './AppBar'
+import {AppProvider} from './AppProvider'
+
+function App() {
+  return (
+      <AppLayout>
+        <AppProvider>
+          <AppBar/>
+          <Welcome />
+        </AppProvider>
+      </AppLayout>
+  );
+}
+
+export default App;
