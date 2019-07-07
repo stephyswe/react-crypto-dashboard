7ced903040b44480e6ce76eb6703f8c058c409be
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-07 12:37:01 +0200

    Dashboard bug fix

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -17,7 +17,9 @@ const ControlButtonElem = styled.div`
     ${props => props.active && css`
         text-shadow: 0px 0px 60px #03ff03;
         color: green;
-        
+    `}
+    ${props => props.hidden && css`
+        display: none;
     `}
 `;
 
@@ -28,8 +30,13 @@ function toProperCase(lower) {
 function ControlButton({name}) {
     return (
         <AppContext.Consumer>
-            {({page, setPage}) => (
-            <ControlButtonElem active={page === name} onClick={() => setPage(name)}>
+            {({firstVisit, page, setPage}) => (
+            <ControlButtonElem
+                active={page === name}
+                onClick={() => setPage(name)}
+                hidden={firstVisit && name === 'dashboard'}
+
+            >
                 {toProperCase(name)}
             </ControlButtonElem>
             )}
